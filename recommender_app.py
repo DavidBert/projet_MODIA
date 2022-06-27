201a5beee2b769e35428d62ba0c8c0e6e1925165
Walterseal <baptiste210831@gmail.com>
2022-06-27 22:49:29 +0200

    chgt de nom de app.py

---
+++ recommender_app.py
@@ -0,0 +1,39 @@
+import gradio as gr
+from nltk import word_tokenize
+from nltk.stem import SnowballStemmer
+import pickle
+
+
+# Interface lemma tokenizer from nltk with sklearn
+class StemTokenizer:
+    ignore_tokens = [",", ".", ";", ":", '"', "``", "''", "`"]
+
+    def __init__(self):
+        self.stemmer = SnowballStemmer("english")
+
+    def __call__(self, doc):
+        return [
+            self.stemmer.stem(t)
+            for t in word_tokenize(doc)
+            if t not in self.ignore_tokens
+        ]
+
+
+tokenizer = StemTokenizer()
+
+
+def is_it_good(comment: str):
+    model_name = "model_randomforest.pkl"
+    model = pickle.load(open(model_name, "rb"))
+
+    if type(comment) is str:
+        comment = [comment]
+    sentiment = model.predict(comment)
+    if sentiment == 1:
+        to_return = f"positive comment!"
+    else:
+        to_return = f"negative comment! "
+    return to_return
+
+
+gr.Interface(fn=is_it_good, inputs=["text"], outputs=["textbox"]).launch()
