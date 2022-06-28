c7ae4ffba16efecc9019c22a1a885ea3c92edcd1
Walterseal <baptiste210831@gmail.com>
2022-06-28 13:36:02 +0200

    chgt du nom de l"image

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,4 @@
-sudo docker build -t container_gradio
+sudo docker build -t container_gradio .
 sudo docker run -d -p 7860:7860 container_gradio
 echo "acces your gradio application here : http://localhost:7860"
 export GRADIO_DOCKER_ID=$(sudo docker ps -aqf "ancestor=container_gradio")
